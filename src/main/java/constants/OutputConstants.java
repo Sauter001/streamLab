ac260b45979a82b3eae25dd3bdc8af5828bfceb9
sauter001 <lsj457@naver.com>
2026-02-04 02:16:05 +0900

    refactor: change OutputConstants to final class

--- src/main/java/constants/OutputConstants.java
+++ src/main/java/constants/OutputConstants.java
@@ -1,6 +1,6 @@
 package constants;
 
-public class OutputConstants {
+public final class OutputConstants {
     public static final long SHORT_DIALOGUE_TIME = 1000; // 2초
     public static final long DEFAULT_DIALOGUE_TIME = 2000; // 2초
     public static final long LONG_DIALOGUE_TIME = 3000; // 3초
